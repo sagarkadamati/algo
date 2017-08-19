21f53966dcfefffb5a39c577c7d937a0a7af9b09
Sagar Kadamati <skadamati@nvidia.com>
2017-08-19 21:53:47 +0530

    Added array c file

---
+++ array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int array[10];
+    int i;
+
+    for (i = 0; i < 10; i++)
+        array[i] = i + 10;
+
+    for (i = 0; i < 10; i++)
+        printf("Value at index %d = %d", i, array[i]);
+
+    return 0;
+}
