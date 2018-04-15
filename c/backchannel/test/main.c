2b6e1bebcbaee319ceb4216c6d7a6841c17f7858
Sagar Kadamati <sagarkadamati@gmail.com>
2018-04-16 00:08:01 +0530

    Added signals

--- c/backchannel/test/main.c
+++ c/backchannel/test/main.c
@@ -8,6 +8,15 @@ void sighandlers(int signal)
             bc_deinit();
             break;
         case SIGSEGV:
+            bc_deinit();
+            break;
+        case SIGINT:
+            break;
+        case SIGILL:
+            break;
+        case SIGABRT:
+            break;
+        case SIGFPE:
             break;
         default:
             break;
@@ -21,8 +30,12 @@ static void init_sighandler()
     sa.sa_handler = sighandlers;
     sa.sa_flags = SA_SIGINFO;
     // sa.sa_flags = /*SA_RESETHAND | */SA_NODEFER;  /* To have or have not */
-    sigaction(SIGSEGV, &sa, NULL);
     sigaction(SIGTERM, &sa, NULL);
+    sigaction(SIGSEGV, &sa, NULL);
+    sigaction(SIGINT, &sa, NULL);
+    sigaction(SIGILL, &sa, NULL);
+    sigaction(SIGABRT, &sa, NULL);
+    sigaction(SIGFPE, &sa, NULL);
 }
 
 int main(void)
