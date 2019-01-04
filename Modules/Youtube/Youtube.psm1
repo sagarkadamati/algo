095b1c0cc2c27f59b96b1e1956d7797a5e33d372
Sagar Kadamati <sagarkadamati@gmail.com>
2019-01-04 21:40:34 +0530

    updated

--- Modules/Youtube/Youtube.psm1
+++ Modules/Youtube/Youtube.psm1
@@ -66,6 +66,7 @@ function YoutubeGetURLs($youtubeURL) {
 
 	if ($youtubeURL -like "*list=*") {
 		$VideoUrls = $($(invoke-WebRequest -uri $youtubeURL).Links | Where-Object {$_.HREF -like "/watch*"} | Where-Object innerText -notmatch ".[0-9]:[0-9]." | Where-Object {$_.innerText.Length -gt 3} | Select-Object innerText,@{Name="URL";Expression={'http://www.youtube.com' + $_.href}} | Where-Object innerText -notlike "*Play all*").URL
+		# $VideoUrls = $($(Invoke-RestMethod $youtubeURL).Links | Where-Object {$_.HREF -like "/watch*"} | Where-Object innerText -notmatch ".[0-9]:[0-9]." | Where-Object {$_.innerText.Length -gt 3} | Select-Object innerText,@{Name="URL";Expression={'http://www.youtube.com' + $_.href}} | Where-Object innerText -notlike "*Play all*").URL
 
 		return $VideoUrls
 	}
