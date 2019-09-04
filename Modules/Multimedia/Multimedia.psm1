373f2aa84ff9a03f140d3643977170308616faab
Sagar Kadamati <sagarkadamati@gmail.com>
2019-09-04 12:15:35 +0530

    renamed

--- Modules/Multimedia/Multimedia.psm1
+++ Modules/Multimedia/Multimedia.psm1
@@ -14,19 +14,25 @@ function GetBhagavataSkandam($dir, $sk, $end) {
 	$count = 1
 	foreach($i in 1..$end ) {
 		try {
-			# echo "http://telugubhagavatam.org/?tebha&Skanda=${sk}&Ghatta=${i}"
-			Invoke-WebRequest -Uri "http://telugubhagavatam.org/?tebha&Skanda=${sk}&Ghatta=${i}" -OutFile "tmp${i}.html"
 			$html = New-Object -ComObject "HTMLFile"
-			$html.IHTMLDocument2_write($(Get-Content "tmp${i}.html" -raw))
+			# echo "http://telugubhagavatam.org/?tebha&Skanda=${sk}&Ghatta=${i}"
+			if (Test-Path "html/${count}.html") {
+				$html.IHTMLDocument2_write($(Get-Content "html/${count}.html" -raw))
+			} else {
+				Invoke-WebRequest -Uri "http://telugubhagavatam.org/?tebha&Skanda=${sk}&Ghatta=${i}" -OutFile "${i}.html"
+				$html.IHTMLDocument2_write($(Get-Content "${i}.html" -raw))
+			}
 			$title = ($html.all.tags("span") | foreach { if ($_.classname -match "ghatta") { $_.outerText} })
 			if ($title -notlike "") {
-				$html.all.tags("p") | foreach { if ($_.classname -match "meaning") { $_.outerText; echo ""} } | Out-File -Encoding utf8 "${count} ${title}.md"
-				Move-Item "tmp${i}.html" "html/${count}.html"
+				$cstr = ($count.ToString()).PadLeft($end.ToString().Length, '0')
+				$html.all.tags("p") | foreach { if ($_.classname -match "meaning") { $_.outerText; echo ""} } | Out-File -Encoding utf8 "${cstr} ${title}.md"
+				if (!(Test-Path "html/${count}.html")) {
+					Move-Item "${i}.html" "html/${count}.html"
+				}
 				$count = $count + 1
 			} else {
-				Remove-Item "tmp${i}.html"
+				Remove-Item "${i}.html"
 			}
-			# Remove-Item "tmp${i}.html"
 		} catch {
 			write-warning 'Skandam $sk Ghatta $1 skt found'
 			break
@@ -37,20 +43,20 @@ function GetBhagavataSkandam($dir, $sk, $end) {
 }
 
 function GetBhagavatam {
-	GetBhagavataSkandam "ప్రథమ స్కంధము" 1 41
-	GetBhagavataSkandam "ద్వితీయ స్కంధము" 2 36
-	GetBhagavataSkandam "తృతీయ స్కంధము" 3 57
-	GetBhagavataSkandam "చతుర్థ స్కంధము" 4 28
-	GetBhagavataSkandam "పంచమ స్కంధము - పూర్వ" 5.1 16
-	GetBhagavataSkandam "పంచమ స్కంధము - ఉత్తర" 5.2 8
-	GetBhagavataSkandam "షష్ఠ స్కంధము" 6 17
-	GetBhagavataSkandam "సప్తమ స్కంధము" 7 17
-	GetBhagavataSkandam "అష్టమ స్కంధము" 8 93
-	GetBhagavataSkandam "నవమ స్కంధము" 9 54
-	GetBhagavataSkandam "దశమ స్కంధము - పూర్వ" 10.1 211
-	GetBhagavataSkandam "దశమ స్కంధము - ఉత్తర" 10.2 91
-	GetBhagavataSkandam "ఏకాదశ స్కంధము" 11 19
-	GetBhagavataSkandam "ద్వాదశ స్కంధము" 12 13
+	GetBhagavataSkandam "01 ప్రథమ స్కంధము" 1 41
+	GetBhagavataSkandam "02 ద్వితీయ స్కంధము" 2 36
+	GetBhagavataSkandam "03 తృతీయ స్కంధము" 3 57
+	GetBhagavataSkandam "04 చతుర్థ స్కంధము" 4 28
+	GetBhagavataSkandam "05 పంచమ స్కంధము - పూర్వ" 5.1 16
+	GetBhagavataSkandam "06 పంచమ స్కంధము - ఉత్తర" 5.2 8
+	GetBhagavataSkandam "07 షష్ఠ స్కంధము" 6 17
+	GetBhagavataSkandam "08 సప్తమ స్కంధము" 7 17
+	GetBhagavataSkandam "09 అష్టమ స్కంధము" 8 93
+	GetBhagavataSkandam "10 నవమ స్కంధము" 9 54
+	GetBhagavataSkandam "11 దశమ స్కంధము - పూర్వ" 10.1 211
+	GetBhagavataSkandam "12 దశమ స్కంధము - ఉత్తర" 10.2 91
+	GetBhagavataSkandam "13 ఏకాదశ స్కంధము" 11 19
+	GetBhagavataSkandam "14 ద్వాదశ స్కంధము" 12 13
 }
 
 Export-ModuleMember -Function ConcatVideos, AutoCropVideos, AnalizeVideos, GenVideos
